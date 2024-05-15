faeff861ebc5f77237939b46aaa4904b5cd5c53b
Swilam <swilaaam@gmail.com>
2024-05-15 15:02:11 +0200

    Initial Implementation

--- resources/views/templates/weavy.blade.php
+++ resources/views/templates/weavy.blade.php
@@ -10,7 +10,7 @@
 {{--    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>--}}
 {{--    <link href="https://fonts.googleapis.com/css2?family=Poetsen+One&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">--}}
 
-    @include('open-graphy::partials.css')
+    @include('open-graphy::partials.styles')
     @include('open-graphy::partials.js')
 
     <script>
